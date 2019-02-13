eeac08d3a5eaa3157fce06fc4ce00080a959383c
claydevlin <46453642+claydevlin@users.noreply.github.com>
2019-02-13 17:52:05 -0500

    Update TestVoteForSweater2.sol

--- TestVoteForSweater2.sol
+++ TestVoteForSweater2.sol
@@ -1,3 +1,12 @@
 pragma solidity 0.4.25;
 import "./VoteForSweater2.sol";
 contract TestVoteForSweater2 is VoteForSweater2 {
+  function testSweater() {
+    uint blueVotes = getVotes("blue");
+    sweaterVote("blue:);
+    blueVotes += 1;
+    assert(getVotes("blue") == blueVotes);
+    assert(getVotes("turtle neck") == 0);
+    }
+  }
+  
