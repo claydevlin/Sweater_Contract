7258d314327c541b7a490864401d362ee3358703
claydevlin <46453642+claydevlin@users.noreply.github.com>
2019-02-26 21:35:21 -0500

    Update VoteForSweater3.sol

--- VoteForSweater3.sol
+++ VoteForSweater3.sol
@@ -4,3 +4,17 @@ contract VoteForSweater3 {
   event sweaterEvent(address sender, string color);
   string[] public colorChoices = ["blue", "green", "red", "brown"];
   mapping(string => bool) alreadyVoted; 
+  function sweaterVote(string memory color) public {
+    require(already voted[msg.sender] == false);
+    for (uint i = 0; i < colorChoices.length; i++) {
+      if(keccak256(colorChoices[i]) == keccak256(color)) {
+        sweaterMap[color] += 1;
+        alreadyVoted[msg.sender] == true;
+        emit sweaterEvent(msg.sender, color);
+        break;
+      }
+    }
+  }
+  function getVotes(string memory color) public view return (uint votes) {
+    return sweaterMap[color];
+  }
